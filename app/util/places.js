69bc31eaebc4bc1ab64d6c258643dd4d1d73e5b5
smax253 <smax253@gmail.com>
2018-10-06 18:19:34 -0400

    Added consistency

--- app/util/places.js
+++ app/util/places.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 String key = '&key=AIzaSyCL2k612OOVYYxrqP2j7t1ty8nANPpQlPE';
 const getPlacesWithinRange = async (location, radius, query) => {
     const res = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?location='+location[0] + ',' + location[1] + '&radius=' + radius + 'sensor=true' + '&query' + query + key);
-    resultList = []
+    resultList = [];
     for(int i = 0; i<res.results.length; i++){
         resultList.push(res.results[i]);
     }
@@ -11,7 +11,12 @@ const getPlacesWithinRange = async (location, radius, query) => {
 const getPlacesCity = async (city, category) =>{
     String query = category + " in " + city;
     const res = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + query + key);
+    resultList = [];
+    for(int i = 0; i<res.results.length; i++){
+        resultList.push(res.results[i]);
+    }
     return res;
+    return resultList;
 }
 const getPlaceDetails = async (id) =>{
     query = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+id+"&fields=opening_hours"+key;
