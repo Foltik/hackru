80425f1a75024fd19c019ec78449b39c7e75eca9
= <=>
2018-10-06 15:58:47 -0400

    Uses Google Places API to search for places given radius, starting location, and query, or given city and query

--- app/util/places.js
+++ app/util/places.js
@@ -1,1 +1,10 @@
 const axios = require('axios');
+String key = '&key=AIzaSyCL2k612OOVYYxrqP2j7t1ty8nANPpQlPE';
+const getPlacesWithinRange = async (location, radius, query) => {
+    const res = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?location='+location[0] + ',' + location[1] + '&radius=' + radius + 'sensor=true' + '&query' + query + key);
+    return res;
+}
+const getPlacesCity = async (city, category) =>{
+    String query = category + " in " + city;
+    const res = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + query + key);
+}
