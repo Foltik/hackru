31a4acfc4653ddc30781bff8c103da677d4ffc53
smax253 <smax253@gmail.com>
2018-10-06 18:15:24 -0400

    Changed getplaceswithinrange to return list of places, added placedetail

--- app/util/places.js
+++ app/util/places.js
@@ -2,9 +2,19 @@ const axios = require('axios');
 String key = '&key=AIzaSyCL2k612OOVYYxrqP2j7t1ty8nANPpQlPE';
 const getPlacesWithinRange = async (location, radius, query) => {
     const res = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?location='+location[0] + ',' + location[1] + '&radius=' + radius + 'sensor=true' + '&query' + query + key);
-    return res;
+    resultList = []
+    for(int i = 0; i<res.results.length; i++){
+        resultList.push(res.results[i]);
+    }
+    return resultList;
 }
 const getPlacesCity = async (city, category) =>{
     String query = category + " in " + city;
     const res = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + query + key);
+    return res;
+}
+const getPlaceDetails = async (id) =>{
+    query = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+id+"&fields=opening_hours"+key;
+    const res = await axios.get(query);
+    return res;
 }
