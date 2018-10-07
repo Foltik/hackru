84fd4cecebf3ef0dd76b8717c0eef59f638b9961
smax253 <smax253@gmail.com>
2018-10-06 22:45:33 -0400

    Enforced javascript rules for variables

--- app/util/places.js
+++ app/util/places.js
@@ -1,25 +1,24 @@
 const axios = require('axios');
-String key = '&key=AIzaSyCL2k612OOVYYxrqP2j7t1ty8nANPpQlPE';
+let key = '&key=AIzaSyCL2k612OOVYYxrqP2j7t1ty8nANPpQlPE';
 const getPlacesWithinRange = async (location, radius, query) => {
     const res = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?location='+location[0] + ',' + location[1] + '&radius=' + radius + 'sensor=true' + '&query' + query + key);
-    resultList = [];
-    for(int i = 0; i<res.results.length; i++){
+    let resultList = [];
+    for(let i = 0; i<res.results.length; i++){
         resultList.push(res.results[i]);
     }
     return resultList;
 }
 const getPlacesCity = async (city, category) =>{
-    String query = category + " in " + city;
+    let query = category + " in " + city;
     const res = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + query + key);
     resultList = [];
-    for(int i = 0; i<res.results.length; i++){
+    for(let i = 0; i<res.results.length; i++){
         resultList.push(res.results[i]);
     }
-    return res;
     return resultList;
 }
 const getPlaceDetails = async (id) =>{
-    query = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+id+"&fields=opening_hours"+key;
+    let query = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+id+"&fields=opening_hours"+key;
     const res = await axios.get(query);
     return res;
 }
